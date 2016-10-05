560a78d5ae559efb8ba457156e16dee69c292c32
Mikkel <whargoul.mikkel@gmail.com>
2016-10-05 12:56:18 +0200

    Solution til udp update

---
+++ Øvelse 9/UDP_Socket/server/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+
+namespace server
+{
+	class Server
+	{
+		IPEndPoint ipep = new IPEndPoint()
+
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+		}
+	}
+}
