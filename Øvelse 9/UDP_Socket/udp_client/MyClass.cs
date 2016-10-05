9f6abaccb98227b9ce47cb6d7d2de8e629674987
Mikkel <whargoul.mikkel@gmail.com>
2016-10-05 12:40:25 +0200

    Solution til udp

---
+++ Øvelse 9/UDP_Socket/udp_client/MyClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace udp_client
+{
+	public class MyClass
+	{
+		public MyClass ()
+		{
+		}
+	}
+}
+
---
+++ Øvelse 9/UDP_Socket/udp_server/MyClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace udp_server
+{
+	public class MyClass
+	{
+		public MyClass ()
+		{
+		}
+	}
+}
+
